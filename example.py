4eed835de2bf7fae415213b9accf15fe5bc6ec40
Punativinesh <91261301+Punativinesh@users.noreply.github.com>
2021-09-29 08:59:10 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+w=('vinesh','vamsi')
+for p in w:
+    print(p,len(p))
+
+
+
---
+++ rock paper scissor.py
@@ -0,0 +1,25 @@
+p1action=input()
+p2action=input()
+if p1action==p2action:
+    print("try another time")
+elif p1action=="rock":
+    if p2action=="scissor":
+        print("congrats p1 on winning the game")
+    else:
+        print("congrats p2 on winning the game")
+elif p1action=="paper":
+    if p2action=="rock":
+        print("congrats p1 on winning the game")
+    else:
+        print("congrats p2 on winning the game")
+elif p1action=="scissor":
+    if p2action=="paper":
+        print("congrats p1 on winning the game")
+    else:
+        print("congrats p2 on winning the game")
+p=input("want to play or not")
+if(p=="yes"):
+    print("start making actions")
+else:
+    print("thank you for playing the game")
+
